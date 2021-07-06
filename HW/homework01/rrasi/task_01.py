c68f2adfe60fa86859a8be1f8db39dc4814cb184
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-06 10:15:58 +0300

    changed path

---
+++ HW/homework01/rrasi/task_01.py
@@ -0,0 +1,17 @@
+# Task 1
+a = int(input('Please enter number a:'))
+b = int(input('Please enter number b:'))
+
+print('a+b=' + str(a+b))
+print('a-b=' + str(a-b))
+print('a*b=' + str(a*b))
+print('a/b=' + str(a/b))
+
+# Task 2
+name = input('What is your name?')
+age = input('How old are you?')
+city = input('Where do you live?')
+
+print('Hello,' + name)
+print('Your age is ' + age)
+print('Your live in 5' + city)
