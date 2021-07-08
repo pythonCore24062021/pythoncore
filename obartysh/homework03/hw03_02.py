cefb3672ecd2ccdb3ff5a5bdf93c5150714da402
obartysh <obartysh@gmail.com>
2021-07-08 12:03:11 +0300

    adding branch and 2 tasks as home work

---
+++ obartysh/homework03/hw03_02.py
@@ -0,0 +1,11 @@
+print("введіть число");
+a = input();
+res = list(map(int, str(a)));
+suma=0;
+for n in range(0, len(res)):
+    suma = suma + res[n]
+print("Sum of all elements in given list: ", suma);
+reversed_list = res[::-1];
+print('reversed List:', reversed_list);
+res.sort();
+print(res);
