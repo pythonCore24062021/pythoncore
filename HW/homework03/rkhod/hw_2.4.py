66a3a211699f89fc4866a4b9da8f03fa9780998c
Roman Khodorovych <r.khodorovych@gmail.com>
2021-07-12 01:06:24 +0300

    add home work

---
+++ HW/homework03/rkhod/hw_2.4.py
@@ -0,0 +1,14 @@
+# Operating with 'vars'
+var_int=10
+var_float=8.4
+var_str="No"
+var_big=3.5*var_int
+print(var_big)
+var_float=var_float-1
+print(var_float)
+var_div1=var_int/var_float
+print(var_div1)
+var_div2=var_big/var_float
+print(var_div2)
+var_str=((var_str*2)+("Yes"*3))
+print(var_str)
