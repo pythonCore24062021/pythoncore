2f1fe48d4d4847e611252ca3bbd69d47ce41de38
Liubomyr Halamaha2 <galamagal86@gmail.com>
2021-07-12 17:48:05 +0300

    lesson 04

---
+++ lessons/lesson04.py
@@ -0,0 +1,88 @@
+a = -1
+b = 0
+c = 1
+# if a > b:
+#     print("a > b")
+#     print(True)
+# print("text")
+# if a < b:
+#     print("a < b")
+#     print(True)
+# print("text")
+# if a > b:
+#     print("true a < b")
+# else:
+#     print(1)
+#     print(2)
+#     print(3)
+# a = -10
+# if a > 0:
+#     print("a > 0")
+# elif a == 0:
+#     print("a == 0")
+# elif a < 0:
+#     print("a < 0")
+
+# a =11
+# if a >= 10:
+#     print("a > 10")
+# elif a > 9:
+#     print("a > 9")
+# elif a > 8:
+#     print("a > 8")
+# elif a > 7:
+#     print("a > 7")
+# else:
+#     print("else")
+# a = 1
+# b = 2
+# c = 0
+# if a == 1 and (b==2 or c ==3):
+#     print("a == 1 and b==2 and c ==0")
+# c = 3
+# if a == 1 and b==2 and c ==3:
+#     print("a == 1 and b==2 and c ==3")
+
+# print(10 == 10)
+# print(10 == 9)
+# print(10 and 0 and [1])
+# print(0 or 0 or [1])
+# print(0 or 0 or [])
+
+# True
+# !0
+
+# if None:
+#     print(True)
+# else:
+#     print(False)
+
+# if True:
+#     if 1:
+#         if 1:
+#             print("3")
+#         else:
+#             print("3")
+#     else:
+#         pass
+# else:
+#     pass
+
+# # c > 10 ? "true" : "false"
+# a = "True" if c > 10 else "False"
+# print(a)
+#
+# if c > 10:
+#     a = "True"
+# else
+#     a = "False"
+
+# print("1");print("5");
+#
+# a = 12+6 \
+#     +hvfhfgcsdvhc sdhgg3
+# print(a)
+name ="Anna"
+age = 18
+
+print(f" my {name}, age: {age -3}")
