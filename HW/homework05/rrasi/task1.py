ccc56d3bf4a9ff840e5e1990e626f96dfabf2f90
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-14 00:13:46 +0300

    hw

--- HW/homework05/rrasi/task1.py
+++ HW/homework05/rrasi/task1.py
@@ -2,4 +2,23 @@ import random
 
 a = random.randint(0, 100)
 print(a)
+i = 0
+while i < 10:
+    n = int(input("Set the number': "))
+    if n > a:
+        print('Your number', n, ' is larger then Random A')
+        i += 1
+        print('Turn number', i)
+    elif n < a:
+        print('Your number', n, ' is less then Random A')
+        i += 1
+        print('Turn number', i)
+    elif n == a:
+        print('Your number', n, ' is correct')
+        i += 1
+        print('Turn number', i)
+        break
+
+
+
 
