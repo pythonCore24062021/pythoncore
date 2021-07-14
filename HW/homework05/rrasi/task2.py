faf7620030b88a69fe529c83ad440f95064d7063
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-14 22:35:42 +0300

    ht3

--- HW/homework05/rrasi/task2.py
+++ HW/homework05/rrasi/task2.py
@@ -1,5 +1,14 @@
 i = 0
-while i < 10:
+while i < 3:
     n = int(input("Set the number': "))
     i += 1
+count = 0
+while n > 0:
+    n, digit = divmod(n, 10)
+    for n in range(digit):
+     if digit % 5 == 0:
+        count +=1
+print(count)
+
+
 
---
+++ HW/homework05/rrasi/task3.py
@@ -0,0 +1,4 @@
+num = int(input("Enter multiplier: "))
+print (num)
+for i in range(1, 10):
+   print(num, 'x', i, '=', num*i)
