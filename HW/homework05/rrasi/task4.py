e465f372b3bdc79982616244853042538270a434
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-14 10:33:32 +0300

    task4

---
+++ HW/homework05/rrasi/task4.py
@@ -0,0 +1,20 @@
+for i in range(10):
+        if i==0 or i==9:
+            for j in range(20):
+                print("-", end=' ')
+        else:
+            print('|',end='')
+            for j in range(1,19):
+                print('*',end=' ')
+            print('',end='|')
+        print()
+
+
+w = int(input("Enter the width: "))
+h = int(input("Enter the height: "))
+for i in range(0, h):
+    print("|",end=' ')
+    for j in range(1, w):
+        print("*", end = " ")
+    else:
+       print("|")
