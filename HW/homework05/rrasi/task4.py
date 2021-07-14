8a9371a9499bcdfbcaa9b6dcd843bbd4bb474899
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-14 14:26:55 +0300

    hw

--- HW/homework05/rrasi/task4.py
+++ HW/homework05/rrasi/task4.py
@@ -14,7 +14,10 @@ w = int(input("Enter the width: "))
 h = int(input("Enter the height: "))
 for i in range(0, h):
     print("|",end=' ')
-    for j in range(1, w):
-        print("*", end = " ")
+    if i == 0 or i == h - 1:
+       for i in range(1, w):
+        print("-", end = " ")
+        if i == 0 or i == w - 1:
+                print(" ", end="|")
     else:
-       print("|")
+       print(" ")
