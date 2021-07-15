ff3aa64c56f9930744d332cbf3d51ea6cf08761a
Ira Letiy <ileti@softserveinc.com>
2021-07-16 01:18:53 +0300

    1,2,3

--- HW/homework03/ileti/hm03ileti.py
+++ HW/homework03/ileti/hm03ileti.py
@@ -1,4 +1,4 @@
-import this
+######1
 python_philosophy = """"
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -23,3 +23,33 @@ Namespaces are one honking great idea -- let's do more of those!
 print(python_philosophy.count("better", "never", "is"))
 print(python_philosophy.title())
 print(python_philosophy.replace("i", "&"))
+
+####2
+number = "4536"
+dobutok = number[0:5]
+dobutok = number[0] * number[1] * number[2] * number[3]
+print("Добуток чотиризначного числа: " + dobutok)
+print(f"Для цього числа {number} Визначте добуток чисел {dobutok})
+
+
+#####3
+a=5
+b=10
+print(a)
+print(b)
+
+a,b  = b, a
+print ("value a: " + a)
+print ("value b: " + b)
+
+######4
+
+Змініть значення, збережене в змінній var_int, збільшивши його в 3.5 рази, результат надайте змінній var_big.
+Змініть значення, збережене в змінній var_float, зменшивши його на одиницю, результат надайте тій же змінній.
+Розділіть var_int на var_float, а потім var_big на var_float. Результат запишіть у змінні var_div1 та var_div2 відповідно.
+Змініть значення змінної var_str на "NoNoYesYesYes". При формуванні нового значення використовуйте операції конкатенації (+) і повторення рядка (*).
+
+
+var_int=10
+var_float=8.4
+var_str="No"
