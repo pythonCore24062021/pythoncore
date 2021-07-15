1e34a1b05ab65a6a5cc4bcd1b8db581f583e19b1
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-15 10:25:59 +0300

    ht2

--- HW/homework05/rrasi/task5.py
+++ HW/homework05/rrasi/task5.py
@@ -1,2 +1,14 @@
+P = 5
+H = 8
+i = 0
+while (n != P) or (n != H):
+    n = int(input("Set the number': "))
+    i += 1
+      if
+        print("suma = " n + P)
 
+      print("Sum: ", suma)
 
+      break
+else:
+   print(suma)
