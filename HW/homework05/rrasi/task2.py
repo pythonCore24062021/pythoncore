136bbf78f4f6c2a63ae93130b0fa3e1e3596bdce
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-18 14:57:16 +0300

    HW5Task2

--- HW/homework05/rrasi/task2.py
+++ HW/homework05/rrasi/task2.py
@@ -1,9 +1,19 @@
 i = 0
+count = 0
 while i < 3:
     n = int(input("Set the number': "))
     i += 1
+    if n % 5 == 0:
+       count += 1
+print(count)
+
+
+i = 0
 count = 0
-for count in range(n):
-     if n % 5 == 0:
-        count +=1
-     print(count)
+while not i == 10:
+    n = int(input(f"Set the {i+1} number': "))
+    if n > 2:
+        i += 1
+        if n % 5 == 0:
+            count += 1
+print(count)
--- HW/homework05/rrasi/task5.py
+++ HW/homework05/rrasi/task5.py
@@ -3,11 +3,15 @@ H = 8
 i = 0
 n = int()
 suma = 0
+dobutok = 1
 while (n != P) and (n != H):
     n = int(input("Set the number': "))
     i += 1
     if n < P:
         suma += n
 #        print("suma = ",  n + P)
+    if n > H:
+        dobutok *= n
 else:
    print(suma)
+   print(dobutok)
