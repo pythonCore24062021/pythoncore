4c824156b5aa7883ff5afc23afa4de71817ef24c
Ruslana Rasiak <rusichka85d@gmail.com>
2021-07-18 15:07:17 +0300

    HW4Task5

--- HW/homework05/rrasi/task5.py
+++ HW/homework05/rrasi/task5.py
@@ -4,6 +4,7 @@ i = 0
 n = int()
 suma = 0
 dobutok = 1
+count = 0
 while (n != P) and (n != H):
     n = int(input("Set the number': "))
     i += 1
@@ -12,6 +13,9 @@ while (n != P) and (n != H):
 #        print("suma = ",  n + P)
     if n > H:
         dobutok *= n
+    if n >= P and n <= H:
+        count +=1
 else:
    print(suma)
    print(dobutok)
+   print(count)
