333e07cfddd3fe52ef22c61deea407d2e3deee32
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-03 19:12:17 +0300

    hw6 task 15

--- HW/homework06/rrasi/task15.py
+++ HW/homework06/rrasi/task15.py
@@ -1,1 +1,34 @@
 #Змінити послідовність стовпців матриці так, щоб елементи її першого рядка були відсортовані за зростанням.
+
+import random
+
+m = 10
+n = 10
+matrix = []
+matrix2 = []
+
+for i in range(m):
+    b = []
+    for j in range(n):
+        b.append(int(random.randint(0, 100)))
+    matrix.append(b)
+sorted_list = sorted(range(len(matrix)), key=lambda i: matrix[0][i])
+
+for i in range(len(matrix)):
+    line = []
+    for j in range(len(matrix[0])):
+        line.append(matrix[i][sorted_list[j]])
+    matrix2.append(line)
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        print(matrix[i][j], end=" ")
+    print()
+print("New matrix")
+for i in range(len(matrix2)):
+    for j in range(len(matrix2[i])):
+        print(matrix2[i][j], end=" ")
+    print()
+
+
+
