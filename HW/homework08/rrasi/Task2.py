32b3f9e98da2b92dc602a028c5d064e1ffaac8a0
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-06 08:43:05 +0300

    'hw8'

---
+++ HW/homework08/rrasi/Task2.py
@@ -0,0 +1,3 @@
+#Визначте атрибути fullname та email в класі Employee. При заданих first та last names:
+#В конструкторі сформуйте fullname звичайним з’єднанням через пробіл first та last name.
+#В конструкторі сформуйте email з’єднанням first та last name через ‘.’ між ними та приєднуючи ‘@company.com’ наприкінці.
---
+++ HW/homework08/rrasi/Task3.py
@@ -0,0 +1,4 @@
+#В класі Name визначте:
+#атрибути для first name та last name (fname та lname відповідно);
+#атрибут fullname що повертає first і last names;
+#атрибут initials який повертає ініціали (перші літери first та last name, розділених ‘.’ .
