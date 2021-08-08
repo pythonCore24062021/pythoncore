8ba1a9373b3256a5a1214c0e05e90641506f740f
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-08 13:46:15 +0300

    hw8task2

--- HW/homework08/rrasi/Task2.py
+++ HW/homework08/rrasi/Task2.py
@@ -3,26 +3,13 @@
 #В конструкторі сформуйте email з’єднанням first та last name через ‘.’ між ними та приєднуючи ‘@company.com’ наприкінці.
 
 class Employee:
-    def __init__(self, fullname, email):
-        self.fullname = fullname
-        self.email = email
-    def __str__(self):
-        return f"name:{self.name} age:{self.age}"
-    def compare_age(self, person):
-        if self.age < person.age:
-            return f"{person.name} is older than {self.name}"
-        if self.age > person.age:
-            return f"{person.name} is younger than {self.name}"
-        if self.age == person.age:
-            return f"{person.name} is the same age as {self.name}"
-my = Person("Ruslana", 25)
-p1 = Person("Ruslana1", 15)
-p2 = Person("Ruslana2", 25)
-p3 = Person("Ruslana3", 35)
-print(my)
-print(p1)
-print(p2)
-print(p3)
-print(my.compare_age(p1))
-print(my.compare_age(p2))
-print(my.compare_age(p3))
+    def __init__(self, first_name, last_name, email="@company.com"):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.fullname = first_name + " " + last_name
+        self.email = first_name + "." + last_name + email
+
+    def __repr__(self):
+        return self.fullname + ": " + self.email
+user1 = Employee("ruslana", "yaniv")
+print(user1)
